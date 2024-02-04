fdaf29d45d2e81e4d5c3fedebb5bb91b5b5098a7
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-02-04 19:26:11 +0200

    fix: web_static full deployment script.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -53,7 +53,8 @@ def do_deploy(archive_path):
         run("rm -rf /data/web_static/current")
         run("mv {}{}/web_static/* {}{}".format(path, name, path, name))
         run("rm -rf {}{}/web_static".format(path, name))
-        run("cp {}{}/103-index.html {}{}/my_index.html".format(path, name, path, name))
+        run("cp {}{}/103-index.html {}{}/my_index.html"
+            .format(path, name, path, name))
         run("ln -sf {}{} /data/web_static/current".format(path, name))
         sudo("service nginx restart")
         return True
